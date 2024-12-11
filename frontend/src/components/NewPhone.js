5a0cda00a2d5c7f841b04b612c88984a5aee21d1
Jefri <22081447@students.latrobe.edu.au>
2024-12-11 16:06:52 +0700

    Modify front end to be compatible with the new model

--- frontend/src/components/NewPhone.js
+++ frontend/src/components/NewPhone.js
@@ -1,51 +1,62 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function NewPhone(props) {
-    const {contact, phones, setPhones} = props;
-    const [number, setNumber] = useState('');
-    const [name, setName] = useState('');
+  const { contact, phones, setPhones } = props;
+  const [phone_number, setNumber] = useState("");
+  const [phone_type, setName] = useState("");
 
-    async function createPhone(e) {
-        e.preventDefault();
+  async function createPhone(e) {
+    e.preventDefault();
 
-        const response = await fetch('http://localhost/api/contacts/' + contact.id + '/phones', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                number,
-                name
-            })
-        });
+    const response = await fetch(
+      "http://localhost/api/contacts/" + contact.id + "/phones",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          phone_number,
+          phone_type,
+        }),
+      }
+    );
 
-        const data = await response.json();
+    const data = await response.json();
 
-        if (data.id) {
-            setPhones([...phones, data]);
-        }
-
-        setNumber('');
-        setName('');
+    if (data.id) {
+      setPhones([...phones, data]);
     }
 
-	return (
-        <form onSubmit={createPhone} onClick={(e) => e.stopPropagation()} className='new-phone'>
-            <select
-                name="name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-            >
-                <option value="-">Select a category</option>
-                <option value="Mobile Phone">Mobile Phone</option>
-                <option value="Work Phone">Work Phone</option>
-                <option value="Telephone">Telephone</option>
-                <option value="Fax">Fax</option>
-            </select>
-            <input type='text' placeholder='Phone Number' onChange={(e) => setNumber(e.target.value)} value={number}/>
-            <button className='button green' type='submit'>Add Jefri's Phone</button>
-        </form>
-	);
+    setNumber("");
+    setName("");
+  }
+
+  return (
+    <form
+      onSubmit={createPhone}
+      onClick={(e) => e.stopPropagation()}
+      className="new-phone"
+    >
+      <select onChange={(e) => setName(e.target.value)} value={phone_type}>
+        <option value="">Select a category</option>
+        <option value="Mobile Phone">Mobile Phone</option>
+        <option value="Work Phone">Work Phone</option>
+        <option value="Telephone">Telephone</option>
+        <option value="Fax">Fax</option>
+      </select>
+
+      <input
+        type="text"
+        placeholder="Phone Number"
+        onChange={(e) => setNumber(e.target.value)}
+        value={phone_number}
+      />
+      <button className="button green" type="submit">
+        Add Bagus’s Phone
+      </button>
+    </form>
+  );
 }
 
 export default NewPhone;
